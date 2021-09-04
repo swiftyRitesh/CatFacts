bae9ef610de5a6d35a529e18e53de094c0b5d2dc
riteshideas <ritesh.sathiyamoorthi@gmail.com>
2021-09-04 09:32:50 +0800

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Ritesh Sathiyamoorthi on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
